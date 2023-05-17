f9a53a2ec0848264b8b4827f5edfe71966acbf3d
Daniel Gu <dgu8957@gmail.com>
2023-05-16 21:28:34 -0700

    Remove python version requirement and torch/torchvision/clip from install_requires.

--- setup.py
+++ setup.py
@@ -8,7 +8,6 @@ setup(
     description="",
     author="thuml",
     packages=find_packages(),
-    python_requires="==3.9",
     install_requires=[
         "accelerate == 0.12.0",
         "absl-py",
@@ -16,9 +15,6 @@ setup(
         "einops",
         "ftfy == 6.1.1",
         "transformers == 4.23.1",
-        "torch",
-        "torchvision",
-        "clip @ git+https://github.com/openai/CLIP.git@main#egg=clip",
     ],
     include_package_data=True,
 )
